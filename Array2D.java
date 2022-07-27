1404a2c9337b796671c1f008ca142f70271aaefb
rootoor-dev <rootoor.projects@gmail.com>
2022-07-27 01:32:51 +0000

    Create Array2D.java

---
+++ Array2D.java
@@ -0,0 +1,27 @@
+/**
+
+Author : ADI Junior
+
+NB: En programmation, tout tableau est d'emblée écrit selon l'ordre majeur des lignes c'est-à-dire ligne après ligne. 
+En mathématique (algèbre linéaire), toute matrice (tableau à deux dimensions) est d'emblée écrite selon l'ordre des lignes 
+également sauf les vecteurs qui doivent impérativement être écrits selon l'ordre des colonnes pour donner en deux dimensions 
+une matrice-colonne ou vecteur-colonne. Ce qui s'obtient en programmation par transposition (transposée du vecteur).
+
+
+NOTES : NB: In programming, any array is immediately written according to the major order of the rows (rows-major order), 
+that means line after line. In mathematics (linear algebra), any array, also called matrix (two-dimensional array) 
+is immediately written according to the order of the rows also except the vectors which must imperatively be written 
+according to the order of the columns (columns-major order) to give in two dimensions a matrix-column or column-vector. 
+What is obtained in programming by transpose (transposed of the vector).
+
+*/
+
+
+public class Array2D {
+  
+  
+  
+  
+  
+  
+}  
